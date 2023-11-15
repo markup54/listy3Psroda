ccd3fe705671827bc504ebef3f2ad5686232de1d
markup54 <marzenakupka@tlen.pl>
2023-11-15 15:10:53 +0100

    produkt klasa w adapterze

--- app/src/main/java/pl/zabrze/zs10/listy3psroda/MainActivity.java
+++ app/src/main/java/pl/zabrze/zs10/listy3psroda/MainActivity.java
@@ -9,18 +9,18 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<String> wszystkieProdukty = new ArrayList<>();
+    private ArrayList<Produkt> wszystkieProdukty = new ArrayList<>();
     private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        wszystkieProdukty.add("niezbadan planeta");
-        wszystkieProdukty.add("poszukiwanie planety x");
-        wszystkieProdukty.add("sowy");
-        wszystkieProdukty.add("maszyna Turinga");
+        wszystkieProdukty.add(new Produkt("niezbadan planeta",200));
+        wszystkieProdukty.add(new Produkt("poszukiwanie planety x",120));
+        wszystkieProdukty.add(new Produkt("sowy",50));
+        wszystkieProdukty.add(new Produkt("maszyna Turinga",130));
         listView = findViewById(R.id.listView2);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+        ArrayAdapter<Produkt> adapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 wszystkieProdukty
